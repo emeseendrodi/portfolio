b1d448f63f6550ae67fe8b383ad49a9cb39d20a0
Endrődi Emese <emeseendrod@gmail.com>
2025-08-06 10:40:15 +0200

    project photos

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,12 +2,19 @@
 import ProjectCard from "./ProjectCard";
 import portfolioImg from '../assets/portfolio.png';
 import limesImg from '../assets/limes.png';
+import jimmysImg from '../assets/Jimmys.png';
 
 export default function Projects() {
  
 
   const projects = [
-
+ {
+      title: "Jimmy' Dinner",
+      image: jimmysImg,
+      description: "My first practice project for Javascript",
+      tech: ["HTML", "CSS", "JavaScript"],
+      github: "https://github.com/emeseendrodi/restaurant-ordering-app",
+    },
     {
       title: "Portfolio",
       image: portfolioImg,
