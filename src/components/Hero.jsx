506b10690ae43c0bfd5083d5de864c746b096cf2
Endrődi Emese <emeseendrod@gmail.com>
2025-08-06 11:09:49 +0200

    resume up

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,18 @@ export default function Hero() {
       <p className="hero-subtitle">
         I'm a Frontend Developer passionate about <span>React</span>, <span>UI/UX</span>,<br></br> and <span>Data Science</span>.
       </p>
+
+      <div className="hero-btn">
       <a href="#projects" className="hero-button">View My Projects</a>
+        <a
+        href="/emese_endrodi_cv_en.pdf"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="hero-button secondary"
+      >
+        Resume 
+      </a>
+      </div>
     </section>
   );
 }
